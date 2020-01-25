2b4dce6eb37f04d7c76f4f877b3c06537b44f7e5
kurakura0147 <45900035+kurakura0147@users.noreply.github.com>
2020-01-26 04:00:26 +0900

    csrf

--- web/resources/js/bootstrap.js
+++ web/resources/js/bootstrap.js
@@ -26,3 +26,18 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
+
+
+import { getCookieValue } from './util'
+
+window.axios = require('axios')
+
+// Ajaxリクエストであることを示すヘッダーを付与する
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+
+window.axios.interceptors.request.use(config => {
+  // クッキーからトークンを取り出してヘッダーに添付する
+  config.headers['X-XSRF-TOKEN'] = getCookieValue('XSRF-TOKEN')
+
+  return config
+})
